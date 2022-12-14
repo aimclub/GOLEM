0833627705072de951bb38fd7d46289b000b4206
Lyubov Yamshchikova <43475193+YamLyubov@users.noreply.github.com>
2022-12-14 15:48:24 +0300

    Add random search optimizer

--- golem/core/optimisers/random/random_mutation_optimizer.py
+++ golem/core/optimisers/random/random_mutation_optimizer.py
@@ -11,6 +11,7 @@ from golem.core.optimisers.opt_history_objects.individual import Individual
 from golem.core.optimisers.optimization_parameters import GraphRequirements
 from golem.core.optimisers.optimizer import GraphOptimizer, GraphGenerationParams
 from golem.core.optimisers.timer import OptimisationTimer
+from golem.core.utilities.grouped_condition import GroupedCondition
 
 
 class RandomMutationSearchOptimizer(GraphOptimizer):
@@ -36,31 +37,40 @@ class RandomMutationSearchOptimizer(GraphOptimizer):
         ])
         super().__init__(objective, initial_graphs, requirements, graph_generation_params, graph_optimizer_parameters)
         self.timer = OptimisationTimer(timeout=self.requirements.timeout)
+        self.current_iteration_num = 0
+        self.stop_optimization = \
+            GroupedCondition(results_as_message=True).add_condition(
+                lambda: self.timer.is_time_limit_reached(self.current_iteration_num),
+                'Optimisation stopped: Time limit is reached'
+            ).add_condition(
+                lambda: requirements.num_of_generations is not None and
+                        self.current_iteration_num >= requirements.num_of_generations,
+                'Optimisation stopped: Max number of iterations reached')
 
-    def optimise(self, objective: ObjectiveFunction):
+    def optimise(self, objective: ObjectiveFunction) -> Graph:
 
         dispatcher = SimpleDispatcher(self.graph_generation_params.adapter)
         evaluator = dispatcher.dispatch(objective, self.timer)
 
-        num_iter = 0
+        self.current_iteration_num = 0
 
         with self.timer as t:
             best = self._eval_initial_individual(evaluator)
-            while not t.is_time_limit_reached(num_iter):
+            while not self.stop_optimization():
                 mutation = Mutation(self.graph_optimizer_params, self.requirements, self.graph_generation_params)
                 new = mutation(best)
                 evaluator([new])
                 if new.fitness > best.fitness:
                     best = new
                     self.log.info(f'Spent time: {round(self.timer.minutes_from_start, 1)} min')
-                    self.log.info(f'Iteration num: {num_iter}')
+                    self.log.info(f'Iteration num: {self.current_iteration_num}')
                     self.log.info(f'Best individual fitness: {self._objective.format_fitness(best.fitness)}')
 
                 self.history.add_to_history([best])
 
-                num_iter += 1
+                self.current_iteration_num += 1
 
-        return self.graph_generation_params.adapter.restore(best)
+        return best.graph
 
     def _eval_initial_individual(self, evaluator: EvaluationOperator) -> Individual:
         initial_individuals = [Individual(graph) for graph in self.initial_graphs]
