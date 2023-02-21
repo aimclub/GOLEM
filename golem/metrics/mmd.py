6b2fa54f24b67493ac76ba3af6823e306179c427
Grigorii <gkirgizov@gmail.com>
2023-02-21 11:09:31 +0300

    Remove pyemd in favor of scipy (#37)
    * Remove pyemd in favor of scipy
    
    * Bump python version to 3.8 in actions and setup
    
    * Fix python version for YAML
    
    Per https://github.com/actions/setup-python/issues/160

--- golem/metrics/mmd.py
+++ golem/metrics/mmd.py
@@ -1,10 +1,12 @@
 import itertools
 import numpy as np
 from scipy.linalg import toeplitz
-import pyemd
+from scipy.stats import wasserstein_distance
 
 
 def emd(x, y, distance_scaling=1.0):
+    """Earth's mover distance (or Wasserstein metric)
+     between 2 probability distributions."""
     support_size = max(len(x), len(y))
     d_mat = toeplitz(range(support_size)).astype(np.float64)
     distance_mat = d_mat / distance_scaling
@@ -17,7 +19,7 @@ def emd(x, y, distance_scaling=1.0):
     elif len(y) < len(x):
         y = np.hstack((y, [0.0] * (support_size - len(y))))
 
-    emd_value = pyemd.emd(x, y, distance_mat)
+    emd_value = wasserstein_distance(x, y, distance_mat)
     return emd_value
 
 
