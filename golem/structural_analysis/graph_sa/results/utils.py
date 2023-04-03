a1041862aae11b3589826c43fadb2eaf1180bf26
Pinchuk Maya <mayaprr@yandex.ru>
2023-04-03 12:08:01 +0300

    remove try-except blocks

--- golem/structural_analysis/graph_sa/results/utils.py
+++ golem/structural_analysis/graph_sa/results/utils.py
@@ -8,11 +8,8 @@ def get_entity_from_str(graph: Graph, entity_str: str) -> Union[GraphNode, Tuple
     """ Gets entity from entity str using graph. """
     if len(entity_str.split('_')) == 2:
         parent_node_idx, child_node_idx = entity_str.split('_')
-        try:
-            parent_node = graph.nodes[int(parent_node_idx)]
-            child_node = graph.nodes[int(child_node_idx)]
-        except IndexError:
-            print('a')
+        parent_node = graph.nodes[int(parent_node_idx)]
+        child_node = graph.nodes[int(child_node_idx)]
         return parent_node, child_node
     else:
         return graph.nodes[int(entity_str)]
