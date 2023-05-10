9c19bb50b1d4980652b1501f0446ef0f88a1a6af
YamLyubov <yamlyubov@gmail.com>
2023-05-10 12:16:43 +0300

    Add statistics and visualization for guacamol

--- examples/molecule_search/guacamol_benchmark.py
+++ examples/molecule_search/guacamol_benchmark.py
@@ -1,11 +1,14 @@
+import json
 from typing import Optional, List
 
 import joblib
 import numpy as np
+import pandas as pd
 from guacamol.assess_goal_directed_generation import assess_goal_directed_generation
 from guacamol.goal_directed_generator import GoalDirectedGenerator
 from guacamol.scoring_function import ScoringFunction
 from joblib import delayed
+from rdkit.Chem import Draw, MolFromSmiles
 from rdkit.Chem.rdchem import BondType
 
 from examples.molecule_search.mol_adapter import MolAdapter
@@ -26,7 +29,9 @@ from golem.core.optimisers.optimizer import GraphGenerationParams
 
 def load_init_population(scoring_function: ScoringFunction,
                          n_jobs: int = -1,
-                         path=".\\data\\shingles\\guacamol_v1_all.smiles"):
+                         path=".\\data\\guacamol_v1_all.smiles"):
+    """ Original code:
+     https://github.com/BenevolentAI/guacamol_baselines/blob/master/graph_ga/goal_directed_generation.py"""
     with open(path, "r") as f:
         smiles_list = f.readlines()
     joblist = [delayed(scoring_function.score)(smile) for smile in smiles_list]
@@ -39,6 +44,7 @@ def load_init_population(scoring_function: ScoringFunction,
 
 
 class GolemMoleculeGenerator(GoalDirectedGenerator):
+    """ You need to download Guacamol all smiles dataset from https://figshare.com/projects/GuacaMol/56639"""
     def __init__(self,
                  requirements: Optional[MolGraphRequirements] = None,
                  graph_gen_params: Optional[GraphGenerationParams] = None,
@@ -113,9 +119,61 @@ class GolemMoleculeGenerator(GoalDirectedGenerator):
         return top_smiles
 
 
+def visualize(path: str):
+    with open(path) as json_file:
+        results = json.load(json_file)
+    print(f"Guacamol version: {results['guacamol_version']} \n"
+          f"Benchmark version: {results['benchmark_suite_version']} \n")
+    results = results['results']
+    for result in results:
+        generated_molecules, scores = [[MolFromSmiles(smile) for smile, score in result['optimized_molecules'][:12]],
+                                       [round(score, 3) for smile, score in result['optimized_molecules'][:12]]]
+        benchmark_name = result['benchmark_name']
+        scores = [f"{benchmark_name} : {score}" for score in scores]
+        image = Draw.MolsToGridImage(generated_molecules,
+                                     legends=scores,
+                                     molsPerRow=min(4, len(generated_molecules)),
+                                     subImgSize=(1000, 1000),
+                                     legendFontSize=50)
+        image.show()
+        image.save(f'{benchmark_name}_results.png')
+
+
+def get_launch_statistics(paths: List[str]):
+    results = []
+    for path in paths:
+        with open(path) as json_file:
+            results.append(json.load(json_file)['results'])
+
+    column_names = ['benchmark', 'mean', 'std', 'min', 'max', 'mean_time']
+
+    df = pd.DataFrame(columns=column_names)
+
+    for bench_num in range(20):
+        benchmark = results[0][bench_num]['benchmark_name']
+        scores = []
+        time_spent = []
+        for result in results:
+            scores.append(result[bench_num]['score'])
+            time_spent.append(result[bench_num]['execution_time'])
+        bench_result = pd.DataFrame(data=[[benchmark,
+                                           np.mean(scores),
+                                           np.std(scores),
+                                           np.min(scores),
+                                           np.max(scores),
+                                           np.mean(time_spent)]],
+                                    columns=column_names)
+        df = pd.concat([df, bench_result], ignore_index=True, axis=0)
+    pd.set_option('display.max_columns', None)
+    print(df)
+
+
 if __name__ == '__main__':
+    # one launch takes more than 24h
     for launch in range(10):
         print(f'\nLaunch_num {launch}\n')
         assess_goal_directed_generation(GolemMoleculeGenerator(),
                                         benchmark_version='v2',
                                         json_output_file=f'output_goal_directed_{launch}.json')
+    visualize('output_goal_directed_1.json')
+    get_launch_statistics([f'output_goal_directed_{launch}.json' for launch in range(4)])
