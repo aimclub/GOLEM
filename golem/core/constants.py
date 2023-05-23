f3d74a7e900d705141ebb9cbf269a03f9a1850fe
Grigorii Kirgizov <gkirgizov@gmail.com>
2023-05-23 14:44:28 +0300

    Minor: move constant to file

--- golem/core/constants.py
+++ golem/core/constants.py
@@ -3,3 +3,5 @@ import numpy as np
 MAX_GRAPH_GEN_ATTEMPTS = 1000
 MAX_TUNING_METRIC_VALUE = np.inf
 MIN_TIME_FOR_TUNING_IN_SEC = 3
+# Min pop size to avoid getting stuck in local maximum during optimization.
+MIN_POP_SIZE = 5
