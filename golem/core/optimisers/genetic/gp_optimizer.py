4d88e647fff9900acea136694a2018de12303f53
Grigorii Kirgizov <gkirgizov@gmail.com>
2023-05-23 15:13:23 +0300

    WIP Implement ReproductionController

--- golem/core/optimisers/genetic/gp_optimizer.py
+++ golem/core/optimisers/genetic/gp_optimizer.py
@@ -3,8 +3,11 @@ from copy import deepcopy
 from random import choice
 from typing import Sequence, Union, Any
 
-from golem.core.constants import MAX_GRAPH_GEN_ATTEMPTS
+import numpy as np
+
+from golem.core.constants import MAX_GRAPH_GEN_ATTEMPTS, EVALUATION_ATTEMPTS_NUMBER, MIN_POP_SIZE
 from golem.core.dag.graph import Graph
+from golem.core.log import default_log
 from golem.core.optimisers.genetic.gp_params import GPAlgorithmParameters
 from golem.core.optimisers.genetic.operators.crossover import Crossover
 from golem.core.optimisers.genetic.operators.elitism import Elitism
@@ -21,8 +24,6 @@ from golem.core.optimisers.opt_history_objects.individual import Individual
 from golem.core.optimisers.optimization_parameters import GraphRequirements
 from golem.core.optimisers.optimizer import GraphGenerationParams
 from golem.core.optimisers.populational_optimizer import PopulationalOptimizer, EvaluationAttemptsError
-
-EVALUATION_ATTEMPTS_NUMBER = 5
 
 
 class EvoGraphOptimizer(PopulationalOptimizer):
@@ -46,6 +47,10 @@ class EvoGraphOptimizer(PopulationalOptimizer):
         self.elitism = Elitism(graph_optimizer_params)
         self.operators = [self.regularization, self.selection, self.crossover,
                           self.mutation, self.inheritance, self.elitism]
+        self.reproducer = ReproductionController(self.selection,
+                                                 self.mutation,
+                                                 self.crossover,
+                                                 graph_optimizer_params)
 
         # Define adaptive parameters
         self._pop_size: PopulationSize = init_adaptive_pop_size(graph_optimizer_params, self.generations)
@@ -101,36 +106,17 @@ class EvoGraphOptimizer(PopulationalOptimizer):
 
         individuals_to_select = self.regularization(self.population, evaluator)
 
-        # Population size controller is aimed
-        # to keep population size as specified in optimizer settings (make ref).
-        # It is a simple proportional controller
-        # that compensates for invalid results each generation
-        # by computing average ratio of valid results.
+        # TODO: move experience somewhere?
+        experience = self.mutation.agent_experience
 
-        total_target_size = len(individuals_to_select)
-        moving_window_size = 10  # TODO: implement
-        next_population = []
-        required_valid_ratio = 0.9
-        mean_success_rate = 1.0  # TODO: it's just first init in Optimizer init
-        min_pop_size = 2
-        while len(next_population) / total_target_size < required_valid_ratio:
-            # Estimate how many individual we need to complete new population
-            #  based on average success rate of valid results
-            residual_size = max(min_pop_size, int(total_target_size - len(next_population) / mean_success_rate))
-            selected_individuals = self.selection(individuals_to_select, residual_size)
+        new_population = self.reproducer.reproduce(individuals_to_select, evaluator)
 
-            # TODO: this function is just transformation
-            #  with corner case of zero-th; handled either there or here
-            new_population = self._spawn_evaluated_population(selected_individuals, evaluator)
-            next_population.extend(new_population)
-
-            # TODO: implement running average if there's a good estimate
-            if len(new_population) > 10:
-                valid_ratio = len(new_population) / residual_size
-                mean_success_rate = (valid_ratio + mean_success_rate) / 2
+        # Adaptive agent experience collection & learning
+        experience.collect_results(new_population)
+        self.mutation.agent.partial_fit(experience)
 
         new_population = self.inheritance(self.population, new_population)
-        # TODO: logically put into inheritance?
+        # TODO: logically put elitism into inheritance?
         new_population = self.elitism(self.generations.best_individuals, new_population)
 
         return new_population
@@ -154,7 +140,6 @@ class EvoGraphOptimizer(PopulationalOptimizer):
         """Reproduce and evaluate new population. If at least one of received individuals
         can not be evaluated then mutate and evaluate selected individuals until a new
         population is obtained or the number of attempts is exceeded."""
-        experience = self.mutation.agent_experience
 
         for i in range(EVALUATION_ATTEMPTS_NUMBER):
             new_population = self.crossover(selected_individuals)
@@ -162,11 +147,102 @@ class EvoGraphOptimizer(PopulationalOptimizer):
             new_population = evaluator(new_population)
             if new_population:
                 # Perform adaptive learning
-                experience.collect_results(new_population)
-                self.mutation.agent.partial_fit(experience)
+                # experience.collect_results(new_population)
+                # self.mutation.agent.partial_fit(experience)
                 return new_population
             else:
-                experience.reset()
+                # TODO: this is problematic:
+                #  mutation collects negative experience... is that ok?
+                # experience.reset()
+                pass
         else:
             # Could not generate valid population; raise an error
             raise EvaluationAttemptsError()
+
+
+class ReproductionController:
+    """
+    Task of the Reproduction Controller is to reproduce population
+    while keeping population size as specified in optimizer settings.
+
+    It implements a simple proportional controller
+    that compensates for invalid results each generation
+    by computing average ratio of valid results.
+    """
+
+    def __init__(self,
+                 selection: Selection,
+                 mutation: Mutation,
+                 crossover: Crossover,
+                 parameters: GPAlgorithmParameters,
+                 required_valid_ratio: float = 0.9,  # TODO: to parameters
+                 ):
+        self.selection = selection
+        self.mutation = mutation
+        self.crossover = crossover
+
+        self._required_valid_ratio = required_valid_ratio
+        self._minimum_valid_ratio = required_valid_ratio * 0.5
+        self._window_size = max(MIN_POP_SIZE, parameters.max_pop_size // 10)
+        self._success_rate_window = np.full(self._window_size, 1.0)
+
+        self._log = default_log(self)
+
+    @property
+    def mean_success_rate(self) -> float:
+        return float(np.mean(self._success_rate_window))
+
+    def reproduce_uncontrolled(self,
+                               population: PopulationT,
+                               pop_size: int,
+                               evaluator: EvaluationOperator,
+                               ) -> PopulationT:
+        """Reproduces and evaluates population (select, crossover, mutate).
+        Doesn't implement any additional checks on population.
+        """
+        selected_individuals = self.selection(population, pop_size)
+        new_population = self.crossover(selected_individuals)
+        new_population = self.mutation(new_population)
+        new_population = evaluator(new_population)
+        return new_population
+
+    def reproduce(self, population: PopulationT, evaluator: EvaluationOperator):
+        """Reproduces and evaluates population (select, crossover, mutate).
+        Implements additional checks on population to ensure that population size
+        follows required population size.
+        """
+        # TODO: ensure next pop size is determined in Inheritance; not here?
+        required_size = len(population)
+        next_population = []
+        for i in range(EVALUATION_ATTEMPTS_NUMBER):
+            # Estimate how many individuals we need to complete new population
+            # based on average success rate of valid results
+            residual_size = required_size - len(next_population)
+            residual_size = max(MIN_POP_SIZE,
+                                int(residual_size / self.mean_success_rate))
+
+            # Reproduce the required number of individuals
+            new_population = self.reproduce_uncontrolled(population, residual_size, evaluator)
+            next_population.extend(new_population)
+
+            # Keep running average of transform success rate (if sample is big enough)
+            if len(new_population) > MIN_POP_SIZE:
+                valid_ratio = len(new_population) / residual_size
+                self._success_rate_window = np.roll(self._success_rate_window, shift=1)
+                self._success_rate_window[0] = valid_ratio
+
+            # Successful return: got enough individuals
+            if len(next_population) >= required_size * self._required_valid_ratio:
+                return next_population
+        else:
+            # If number of evaluation attempts is exceeded return a warning or raise exception
+            helpful_msg = ('Check objective, constraints and evo operators. '
+                           'Possibly they return too few valid individuals.')
+
+            if len(next_population) >= required_size * self._minimum_valid_ratio:
+                self._log.warning(f'Could not achieve required population size: '
+                                  f'have {len(next_population)}, required {required_size}!\n'
+                                  + helpful_msg)
+            else:
+                raise EvaluationAttemptsError('Could not collect valid individuals'
+                                              ' for next population.' + helpful_msg)
