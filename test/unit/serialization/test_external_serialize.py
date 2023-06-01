e7255d568e3214dfbdac0c2c6979da29626ff3fa
Grigorii Kirgizov <gkirgizov@gmail.com>
2023-06-01 12:35:29 +0300

    Reproduced serialize issue

---
+++ test/unit/serialization/test_external_serialize.py
@@ -0,0 +1,28 @@
+import os.path
+
+import pytest
+
+from golem.core.optimisers.opt_history_objects.opt_history import OptHistory
+from golem.core.paths import project_root
+
+
+@pytest.mark.parametrize('history_path', [
+    'test/data/history_composite_bn_healthcare.json',
+])
+def test_external_history_load(history_path):
+    """The idea is that external histories must be loadable by GOLEM.
+    External histories are those by projects that depend on GOLEM, e.g. BAMT.
+
+    This is needed so that GOLEM could be used as a stand-alone
+    analytic tool for external histories. Or, or example, external histories
+    could be used in FEDOT.Web that depends only on GOLEM.
+    """
+    history_path = project_root() / history_path
+
+    assert os.path.exists(history_path)
+
+    # Expect load without errors
+    history: OptHistory = OptHistory.load(history_path)
+
+    assert history is not None
+    assert len(history.individuals) > 0
