072f027199df2c25248a00edb5bc82334ca473fb
Pinchuk Maya <57305249+maypink@users.noreply.github.com>
2023-06-09 14:14:02 +0300

    NeuralMAB (#102)
    * fix
    
    * fix#2
    
    * minor
    
    * initial neural mab
    
    * add context agents enum
    
    * add experiments
    
    * launch
    
    * minor
    
    * add contextual mab to pull arms
    
    * put NN in a separate class & add docstrings
    
    * adjust settings
    
    * change places of mutations
    
    * fixes after review
    
    * minors
    
    * minors
    
    * add contextual bandits
    
    * add experimenter & multiple fitness lines visualizer
    
    * experiments
    
    * probabilities for contexts
    
    * minors & docstrings
    
    * fix pep8
    
    * fix requirements
    
    * minor
    
    * minor
    
    * add average visualizatio
    
    * fixes after review
    
    * minors
    
    * update requirements
    
    * Update unit-build.yml
    
    * Update unit-build.yml
    
    * fix pep8
    
    * Update unit-build.yml

---
+++ experiments/mab/mab_synthetic_experiment_helper.py
@@ -0,0 +1,208 @@
+import os.path
+import random
+from datetime import timedelta
+from functools import partial
+from pprint import pprint
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from typing import List, Callable, Sequence, Optional, Dict
+
+import networkx as nx
+from matplotlib import pyplot as plt
+from sklearn.cluster import MiniBatchKMeans
+
+from examples.adaptive_optimizer.mab_experiment_different_targets import get_graph_gp_params
+from examples.adaptive_optimizer.utils import plot_action_values
+from examples.synthetic_graph_evolution.graph_search import graph_search_setup
+from examples.synthetic_graph_evolution.utils import draw_graphs_subplots
+from golem.core.adapter.nx_adapter import BaseNetworkxAdapter
+from golem.core.dag.graph import Graph
+from golem.core.optimisers.adaptive.operator_agent import MutationAgentTypeEnum
+
+from golem.core.optimisers.genetic.gp_optimizer import EvoGraphOptimizer
+from golem.core.optimisers.genetic.operators.operator import PopulationT
+from golem.core.optimisers.objective import Objective
+from golem.core.optimisers.optimizer import GraphOptimizer
+from golem.core.paths import project_root
+from golem.visualisation.opt_history.fitness_line import MultipleFitnessLines
+
+
+class MABSyntheticExperimentHelper:
+    """ Class to provide synthetic experiments without data to compare MABs. """
+
+    def __init__(self, launch_num: int, timeout: float, bandits_to_compare: List[MutationAgentTypeEnum],
+                 path_to_save: str = None, is_visualize: bool = False, n_clusters: Optional[int] = None):
+        self.launch_num = launch_num
+        self.timeout = timeout
+        self.bandits_to_compare = bandits_to_compare
+        self.bandit_metrics = dict.fromkeys(bandit.name for bandit in self.bandits_to_compare)
+        self.path_to_save = path_to_save or os.path.join(project_root(), 'mab')
+        self.is_visualize = is_visualize
+        self.histories = dict.fromkeys([bandit.name for bandit in self.bandits_to_compare])
+        self.cluster = MiniBatchKMeans(n_clusters=n_clusters)
+
+    def compare_bandits(self, setup_parameters: Callable, initial_population_func: Callable = None):
+        results = dict()
+        for i in range(self.launch_num):
+            initial_graphs = initial_population_func()
+            for bandit in self.bandits_to_compare:
+                optimizer, objective = setup_parameters(initial_graphs=initial_graphs, bandit_type=bandit)
+                agent = optimizer.mutation.agent
+                result = self.launch_bandit(bandit_type=bandit, optimizer=optimizer, objective=objective)
+                if bandit.name not in results.keys():
+                    results[bandit.name] = []
+                results[bandit.name].append(result)
+        if self.is_visualize:
+            self.show_average_action_probabilities(show_action_probabilities=results, actions=agent.actions)
+
+    def launch_bandit(self, bandit_type: MutationAgentTypeEnum, optimizer: GraphOptimizer, objective: Callable):
+
+        stats_action_value_log: Dict[int, List[List[float]]] = dict()
+
+        def log_action_values(next_pop: PopulationT, optimizer: EvoGraphOptimizer):
+            values = optimizer.mutation.agent.get_action_values(obs=next_pop[0])
+            if 0 not in stats_action_value_log.keys():
+                stats_action_value_log[0] = []
+            stats_action_value_log[0].append(list(values))
+
+        def log_action_values_with_clusters(next_pop: PopulationT, optimizer: EvoGraphOptimizer):
+            obs_contexts = optimizer.mutation.agent.get_context(next_pop)
+            self.cluster.partial_fit(np.array(obs_contexts).reshape(-1, 1))
+            centers = self.cluster.cluster_centers_
+            for i, center in enumerate(sorted(centers)):
+                values = optimizer.mutation.agent.get_action_values(obs=[center])
+                if i not in stats_action_value_log.keys():
+                    stats_action_value_log[i] = []
+                stats_action_value_log[i].append(list(values))
+
+        # set iteration callback
+        if bandit_type == MutationAgentTypeEnum.bandit:
+            optimizer.set_iteration_callback(log_action_values)
+        elif bandit_type in (MutationAgentTypeEnum.contextual_bandit, MutationAgentTypeEnum.neural_bandit):
+            optimizer.set_iteration_callback(log_action_values_with_clusters)
+        else:
+            raise ValueError("No callback function was specified for that bandit type.")
+
+        found_graphs = optimizer.optimise(objective)
+        found_graph = found_graphs[0] if isinstance(found_graphs, Sequence) else found_graphs
+        history = optimizer.history
+        if not self.histories[bandit_type.name]:
+            self.histories[bandit_type.name] = []
+        self.histories[bandit_type.name].append(history)
+        agent = optimizer.mutation.agent
+        found_nx_graph = BaseNetworkxAdapter().restore(found_graph)
+        final_metrics = objective(found_nx_graph).value
+        if not self.bandit_metrics[bandit_type.name]:
+            self.bandit_metrics[bandit_type.name] = []
+        self.bandit_metrics[bandit_type.name].append(final_metrics)
+
+        print('History of action probabilities:')
+        pprint(stats_action_value_log)
+        if self.is_visualize:
+            self.show_fitness_line(found_nx_graph=found_nx_graph, final_metrics=final_metrics,
+                                   history=history)
+            self.show_action_probabilities(bandit_type=bandit_type, stats_action_value_log=stats_action_value_log,
+                                           actions=agent.actions)
+
+        return stats_action_value_log
+
+    @staticmethod
+    def show_fitness_line(found_nx_graph, final_metrics, history):
+        draw_graphs_subplots(found_nx_graph, titles=[f'Found Graph (fitness={final_metrics})'])
+        history.show.fitness_line()
+
+    def show_action_probabilities(self, bandit_type: MutationAgentTypeEnum, stats_action_value_log,
+                                  actions, is_average: bool = False):
+        if is_average:
+            titles = ['Average action Expectation Values', 'Average action Probabilities']
+        else:
+            titles = ['Action Expectation Values', 'Action Probabilities']
+        if bandit_type == MutationAgentTypeEnum.bandit:
+            plot_action_values(stats=stats_action_value_log[0], action_tags=actions, titles=titles)
+            plt.show()
+        else:
+            centers = sorted(self.cluster.cluster_centers_)
+            for i in range(self.cluster.n_clusters):
+                titles = [title + f' for cluster with center {int(centers[i])}' for title in titles]
+                plot_action_values(stats=stats_action_value_log[i], action_tags=actions,
+                                   titles=titles)
+                plt.show()
+
+    def show_average_action_probabilities(self, show_action_probabilities: dict, actions):
+        """ Shows action probabilities across several launches. """
+        for bandit in list(show_action_probabilities.keys()):
+            total_sum = None
+            for launch in show_action_probabilities[bandit]:
+                if not total_sum:
+                    total_sum = launch
+                    continue
+                for cluster in launch.keys():
+                    for i in range(len(total_sum[cluster])):
+                        for j in range(len(total_sum[cluster][i])):
+                            total_sum[cluster][i][j] += launch[cluster][i][j]
+            for cluster in total_sum.keys():
+                for i in range(len(total_sum[cluster])):
+                    for j in range(len(total_sum[cluster][i])):
+                        total_sum[cluster][i][j] /= len(show_action_probabilities[bandit])
+            self.show_action_probabilities(bandit_type=MutationAgentTypeEnum(bandit),
+                                           stats_action_value_log=total_sum,
+                                           actions=actions,
+                                           is_average=True)
+
+    def show_boxplots(self):
+        sns.boxplot(data=pd.DataFrame(self.bandit_metrics))
+        plt.title('Metrics', fontsize=15)
+        plt.show()
+
+    def show_fitness_lines(self):
+        multiple_fitness_lines = MultipleFitnessLines(histories_to_compare=self.histories)
+        multiple_fitness_lines.visualize()
+
+
+def setup_parameters(initial_graphs: List[Graph], bandit_type: MutationAgentTypeEnum,
+                     target_size: int, trial_timeout: float):
+    objective = Objective({'graph_size': lambda graph: abs(target_size -
+                                                           graph.number_of_nodes())})
+
+    # Build the optimizer
+    optimizer, _ = graph_search_setup(
+        objective=objective,
+        optimizer_cls=EvoGraphOptimizer,
+        algorithm_parameters=get_graph_gp_params(objective=objective,
+                                                 adaptive_mutation_type=bandit_type),
+        timeout=timedelta(minutes=trial_timeout),
+        num_iterations=target_size * 3,
+        initial_graphs=initial_graphs
+    )
+    return optimizer, objective
+
+
+def initial_population_func(graph_size: List[int] = None, pop_size: int = None, initial_graphs: List[Graph] = None):
+    if initial_graphs:
+        return initial_graphs
+    initial_graphs = [nx.random_tree(graph_size[i], create_using=nx.DiGraph)
+                      for i in range(pop_size)]
+    return initial_graphs
+
+
+if __name__ == '__main__':
+    timeout = 0.3
+    launch_num = 1
+    target_size = 50
+
+    bandits_to_compare = [MutationAgentTypeEnum.contextual_bandit]
+    setup_parameters_func = partial(setup_parameters, target_size=target_size, trial_timeout=timeout)
+    initial_population_func = partial(initial_population_func,
+                                      graph_size=[random.randint(5, 10) for _ in range(10)] +
+                                                 [random.randint(90, 95) for _ in range(10)],
+                                      pop_size=20)
+
+    helper = MABSyntheticExperimentHelper(timeout=timeout, launch_num=launch_num, bandits_to_compare=bandits_to_compare,
+                                          n_clusters=2, is_visualize=True)
+    helper.compare_bandits(initial_population_func=initial_population_func,
+                           setup_parameters=setup_parameters_func)
+    # helper.show_boxplots()
+    # helper.show_fitness_lines()
---
+++ golem/core/optimisers/adaptive/context_agents.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+from typing import List, Callable, Any
+
+from karateclub import FeatherGraph
+
+from golem.core.adapter.nx_adapter import BanditNetworkxAdapter
+from golem.core.optimisers.opt_history_objects.individual import Individual
+
+
+def feather_graph(obs: Any) -> List[float]:
+    """ Returns embedding based on an implementation of `"FEATHER-G" <https://arxiv.org/abs/2005.07959>`_.
+    The procedure uses characteristic functions of node features with random walk weights to describe
+    node neighborhoods. These node level features are pooled by mean pooling to
+    create graph level statistics. """
+    descriptor = FeatherGraph()
+    nx_graph = BanditNetworkxAdapter().restore(obs)
+    descriptor.fit([nx_graph])
+    return descriptor.get_embedding()[:20]
+
+
+def nodes_num(obs: Any) -> int:
+    """ Returns number of nodes in graph. """
+    if isinstance(obs, Individual):
+        return len(obs.graph.nodes)
+    else:
+        return len(obs.nodes)
+
+
+class ContextAgentTypeEnum(Enum):
+    feather_graph = 'feather_graph'
+    nodes_num = 'nodes_num'
+
+
+class ContextAgentsRepository:
+    """ Repository of functions to encode observations. """
+    _agents_implementations = {
+        ContextAgentTypeEnum.feather_graph: feather_graph,
+        ContextAgentTypeEnum.nodes_num: nodes_num
+    }
+
+    @staticmethod
+    def agent_class_by_id(agent_id: ContextAgentTypeEnum) -> Callable:
+        return ContextAgentsRepository._agents_implementations[agent_id]
