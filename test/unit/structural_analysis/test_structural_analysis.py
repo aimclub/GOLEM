29e0e1845925cfbde123f09c36659f9e10df5f90
Pinchuk Maya <mayaprr@yandex.ru>
2023-06-14 14:10:55 +0300

    update tests

--- test/unit/structural_analysis/test_structural_analysis.py
+++ test/unit/structural_analysis/test_structural_analysis.py
@@ -2,24 +2,6 @@ from golem.core.optimisers.graph import OptNode, OptGraph
 from golem.structural_analysis.graph_sa.graph_structural_analysis import GraphStructuralAnalysis
 
 
-def get_three_depth_manual_class_pipeline():
-    logit_node_primary = OptNode('logit')
-    xgb_node_primary = OptNode('xgboost')
-    xgb_node_primary_second = OptNode('xgboost')
-
-    logit_node_primary2 = OptNode('logit')
-    xgb_node_primary2 = OptNode('xgboost')
-
-    qda_node_third = OptNode('qda', nodes_from=[xgb_node_primary_second, logit_node_primary2, xgb_node_primary2])
-    knn_node_third = OptNode('knn', nodes_from=[logit_node_primary, xgb_node_primary])
-
-    knn_root = OptNode('knn', nodes_from=[qda_node_third, knn_node_third])
-
-    graph = OptGraph(knn_root)
-
-    return graph
-
-
 def get_graph_with_duplicate_operations():
     node_scaling = OptNode('scaling')
     node_pf = OptNode('poly_features', nodes_from=[node_scaling])
