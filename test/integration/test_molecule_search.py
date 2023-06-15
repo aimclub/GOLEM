6ea72d8606e2f68bfd3685fd2079ee6afe61f102
YamLyubov <yamlyubov@gmail.com>
2023-06-15 12:52:42 +0300

    Fix tests

--- test/integration/test_molecule_search.py
+++ test/integration/test_molecule_search.py
@@ -7,7 +7,7 @@ from examples.molecule_search.mol_adapter import MolAdapter
 @pytest.mark.parametrize('metric_name, metric', get_all_mol_metrics().items())
 def test_molecule_search_example(metric_name, metric):
     initial_molecule = get_methane()
-    optimizer, objective = molecule_search_setup(num_iterations=10,
+    optimizer, objective = molecule_search_setup(num_iterations=20,
                                                  metrics=[metric_name],
                                                  initial_molecules=[initial_molecule])
     found_graphs = optimizer.optimise(objective)
