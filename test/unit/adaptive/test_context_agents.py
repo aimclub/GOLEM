6cb4ab7c04179a213860a7de070d6443ee5a8934
Pinchuk Maya <mayaprr@yandex.ru>
2023-06-27 15:54:23 +0300

    minor

--- test/unit/adaptive/test_context_agents.py
+++ test/unit/adaptive/test_context_agents.py
@@ -16,7 +16,7 @@ def get_opt_graph():
 
 
 @pytest.mark.parametrize('context_agent_enum, result_encoding',
-                         [(ContextAgentTypeEnum.operations_encoding,
+                         [(ContextAgentTypeEnum.operations_quantity,
                            [1, 2, 1, 1, 1]),
                           (ContextAgentTypeEnum.labeled_edges,
                            [3, 4, 1, 4, 2, 4, 0, 3, 1, 3]),
