452b884205ca53e1ecc7faec3b282c42450f3f1b
Grigorii Kirgizov <gkirgizov@gmail.com>
2023-06-29 21:36:48 +0300

    WIP minor fixes

--- golem/core/optimisers/opt_history_objects/opt_history.py
+++ golem/core/optimisers/opt_history_objects/opt_history.py
@@ -99,9 +99,8 @@ class OptHistory:
             last_gen = self.generations[last_gen_id]
             for individual in last_gen:
                 ind_path = Path(save_dir, str(last_gen_id), str(individual.uid))
-                if not os.path.isdir(ind_path):
-                    os.makedirs(ind_path)
-                individual.save(json_file_path=Path(ind_path, f'{str(individual.uid)}.json'))
+                ind_path.mkdir(exist_ok=True)
+                individual.save(json_file_path=ind_path / f'{str(individual.uid)}.json')
         except Exception as ex:
             self._log.exception(ex)
 
