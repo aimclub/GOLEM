21843dc51e98dc1b7840870ea5e7c07f6235a295
Lyubov Yamshchikova <YamLyubov@gmail.com>
2023-08-29 13:35:22 +0300

    Pep8

--- examples/molecule_search/mol_transformer/transformer.py
+++ examples/molecule_search/mol_transformer/transformer.py
@@ -29,13 +29,12 @@ class PositionalEncoder(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.dropout = nn.Dropout(p=dropout)
-        # create constant 'pe' matrix with values dependant on 
-        # pos and i
+        # create constant 'pe' matrix with values dependant on pos and i
         pe = torch.zeros(max_seq_len, d_model)
         for pos in range(max_seq_len):
             for i in range(0, d_model, 2):
-                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
-                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1)) / d_model)))
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
@@ -54,17 +53,14 @@ class PositionalEncoder(nn.Module):
         # print(x.mean(), x)
         return x
 
-    
+
 class Norm(nn.Module):
     def __init__(self, d_model, eps=1e-6):
         super().__init__()
-    
         self.size = d_model
-        
         # create two learnable parameters to calibrate normalisation
         self.alpha = nn.Parameter(torch.ones(self.size))
         self.bias = nn.Parameter(torch.zeros(self.size))
-        
         self.eps = eps
     
     def forward(self, x):
@@ -73,51 +69,39 @@ class Norm(nn.Module):
 
 
 def attention(q, k, v, d_k, mask=None, dropout=None):
-    
     scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
-    
     if mask is not None:
         mask = mask.unsqueeze(1)
         scores = scores.masked_fill(mask == 0, -1e9)
-    
     scores = F.softmax(scores, dim=-1)
-    
     if dropout is not None:
         scores = dropout(scores)
-        
     output = torch.matmul(scores, v)
     return output
 
-    
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, heads, d_model, dropout=0.1):
         super().__init__()
-        
         self.d_model = d_model
         self.d_k = d_model // heads
         self.h = heads
-        
         self.q_linear = nn.Linear(d_model, d_model)
         self.v_linear = nn.Linear(d_model, d_model)
         self.k_linear = nn.Linear(d_model, d_model)
-        
         self.dropout = nn.Dropout(dropout)
         self.out = nn.Linear(d_model, d_model)
-    
+
     def forward(self, q, k, v, mask=None):
-        
         bs = q.size(0)
-        
         # perform linear operation and split into N heads
         k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
         q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
         v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
-        
         # transpose to get dimensions bs * N * sl * d_model
         k = k.transpose(1, 2)
         q = q.transpose(1, 2)
         v = v.transpose(1, 2)
-
         # calculate attention using function we will define next
         scores = attention(q, k, v, self.d_k, mask, self.dropout)
         # concatenate heads and put through final linear layer
@@ -129,8 +113,7 @@ class MultiHeadAttention(nn.Module):
 
 class FeedForward(nn.Module):
     def __init__(self, d_model, d_ff=2048, dropout=0.1):
-        super().__init__() 
-    
+        super().__init__()
         # We set d_ff as a default to 2048
         self.linear_1 = nn.Linear(d_model, d_ff)
         self.dropout = nn.Dropout(dropout)
@@ -168,11 +151,11 @@ class DecoderLayer(nn.Module):
         self.norm_1 = Norm(d_model)
         self.norm_2 = Norm(d_model)
         self.norm_3 = Norm(d_model)
-        
+
         self.dropout_1 = nn.Dropout(dropout)
         self.dropout_2 = nn.Dropout(dropout)
         self.dropout_3 = nn.Dropout(dropout)
-        
+
         self.attn_1 = MultiHeadAttention(heads, d_model, dropout=dropout)
         self.attn_2 = MultiHeadAttention(heads, d_model, dropout=dropout)
         self.ff = FeedForward(d_model, dropout=dropout)
