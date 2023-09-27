a2cfbde13e4e8471c19bfc4b862b863863c20121
Sergey <kus7466@gmail.com>
2023-09-27 15:10:17 +0300

    Fix `n_jobs` processing (#202)
    * Add checks to `determine_n_jobs` and add test for it

--- golem/core/optimisers/genetic/evaluation.py
+++ golem/core/optimisers/genetic/evaluation.py
@@ -280,8 +280,14 @@ class SequentialDispatcher(BaseGraphEvaluationDispatcher):
 
 
 def determine_n_jobs(n_jobs=-1, logger=None):
-    if n_jobs > cpu_count() or n_jobs == -1:
-        n_jobs = cpu_count()
+    cpu_num = cpu_count()
+    if n_jobs > cpu_num:
+        n_jobs = cpu_num
+    elif n_jobs <= 0:
+        if n_jobs <= -cpu_num - 1 or n_jobs == 0:
+            raise ValueError(f"Unproper `n_jobs` = {n_jobs}. "
+                             f"`n_jobs` should be between ({-cpu_num}, {cpu_num}) except 0")
+        n_jobs = cpu_num + 1 + n_jobs
     if logger:
         logger.info(f"Number of used CPU's: {n_jobs}")
     return n_jobs
