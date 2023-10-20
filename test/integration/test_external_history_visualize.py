2838b4ee29c71a56eadbf457ef75ff3688c2c373
Pinchuk Maya <mayaprr@yandex.ru>
2023-10-20 17:37:14 +0300

    add test for visualizations

--- test/integration/test_external_history_visualize.py
+++ test/integration/test_external_history_visualize.py
@@ -2,18 +2,19 @@ from pathlib import Path
 
 import pytest
 
-from golem.core.optimisers.opt_history_objects.opt_history import OptHistory
+from golem.core.optimisers.opt_history_objects.opt_history import OptHistory, lighten_history
 from golem.core.paths import project_root
 from golem.visualisation.opt_viz import PlotTypesEnum, OptHistoryVisualizer
 
 
-@pytest.mark.parametrize('history_path', [
-    'test/data/history_composite_bn_healthcare.json',
-])
+@pytest.mark.parametrize('history_path', ['test/data/history_composite_bn_healthcare.json'])
 @pytest.mark.parametrize('plot_type', PlotTypesEnum)
-def test_visualizations_for_external_history(tmp_path, history_path, plot_type):
+@pytest.mark.parametrize('is_light_history', [True, False])
+def test_visualizations_for_external_history(tmp_path, history_path, plot_type, is_light_history):
     history_path = project_root() / history_path
     history = OptHistory.load(history_path)
+    if is_light_history:
+        history = lighten_history(history)
     save_path = Path(tmp_path, plot_type.name)
     save_path = save_path.with_suffix('.gif') if plot_type is PlotTypesEnum.operations_animated_bar \
         else save_path.with_suffix('.png')
