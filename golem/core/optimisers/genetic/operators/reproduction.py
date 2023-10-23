835d3260df757908b8707b25e40407bd72011383
Sergey Kasyanov <kus7466@gmail.com>
2023-10-23 12:45:21 +0300

    wip

--- golem/core/optimisers/genetic/operators/reproduction.py
+++ golem/core/optimisers/genetic/operators/reproduction.py
@@ -1,3 +1,4 @@
+
 from golem.core.constants import MAX_GRAPH_GEN_ATTEMPTS_AS_POP_SIZE_MULTIPLIER
 from golem.core.dag.graph_verifier import GraphVerifier
 from golem.core.log import default_log
@@ -57,79 +58,65 @@ class ReproductionController:
 
         # mutations counters
         mutation_types = self.parameters.mutation_types
-        mutation_count = {mutation_type: 0 for mutation_type in mutation_types}
-        mutation_count_for_each_ind = {descriptive_id: copy(mutation_count)
+        mutation_count_for_each_ind = {descriptive_id: {mutation_type: 0 for mutation_type in mutation_types}
                                        for descriptive_id in population_descriptive_ids_mapping}
-        mutation_tries_for_each_ind = {descriptive_id: copy(mutation_count)
+        all_mutations_count_for_each_ind = {descriptive_id: 0 for descriptive_id in population_descriptive_ids_mapping}
+        mutation_tries_for_each_ind = {descriptive_id: {mutation_type: 0 for mutation_type in mutation_types}
                                        for descriptive_id in population_descriptive_ids_mapping}
 
         # prepare one mutation for each individual in population
-        # mutation_type is None, let Mutation() choose
-        mutation_queue = Queue()
+        mutation_queue = Manager().Queue()
         for descriptive_id, individual in zip(population_descriptive_ids_mapping, population):
+            # mutation_type is None, let Mutation() choose
             mutation_queue.put((descriptive_id, individual, None))
 
-        # run infinite cycle with evaluation in parallel
+        # run cycle with evaluation in parallel
+        # made with joblib.Parallel due to
+        #     it is simple
+        #     it is reliable (joblib/loky solves some problems with multiprocessing)
+        #     joblib is in requirements
         new_population = list()
         with Parallel(n_jobs=self.mutation.requirements.n_jobs, return_as='generator') as parallel:
-            ind_generator = parallel(delayed(mutation_fun)(mutation_queue) for _ in [1] * 5) # cycle([1]))
+            ind_generator = parallel(delayed(mutation_fun)(mutation_queue) for _ in range(max_tries * 2))
             for try_num, (parent_descriptive_id, mutation_type, new_ind) in enumerate(ind_generator):
+                if parent_descriptive_id is None:
+                    continue
                 mutation_tries_for_each_ind[parent_descriptive_id][mutation_type] += 1
                 if new_ind:
+                    mutation_count_for_each_ind[parent_descriptive_id][mutation_type] += 1
+                    all_mutations_count_for_each_ind[parent_descriptive_id] += 1
+
                     descriptive_id = new_ind.graph.descriptive_id
                     if descriptive_id not in self._pop_graph_descriptive_ids:
+                        # add ind to new population
                         new_population.append(new_ind)
                         self._pop_graph_descriptive_ids.add(descriptive_id)
                         if len(new_population) >= target_pop_size:
                             break
 
-                        # count mutations
-                        mutation_count_for_each_ind[parent_descriptive_id][mutation_type] += 1
-                        mutation_count[mutation_type] += 1
-
-                        # choose new mutation for individual, rely on check probabilities
+                        # filter mutations for individual, rely on probabilities
+                        # place for error if mutation_types order in _operator_agent and in mutation_types is differ
+                        allowed_mutation_types = []
                         mutation_probabilities = self.mutation._operator_agent.get_action_probs()
-                        # potential place for error if order in
-                        # mutation_probabilities and mutation_real_propabilities differ
-                        # needs to fix with all mabs and random agent
-                        mutation_real_probabilities = [mutation_count / self.parameters.pop_size
-                                                       for mutation_count in mutation_count.values()]
+                        for mutation_type, mutation_probability in zip(mutation_types, mutation_probabilities):
+                            real_prob = (mutation_count_for_each_ind[parent_descriptive_id][mutation_type] /
+                                         all_mutations_count_for_each_ind[parent_descriptive_id])
+                            if real_prob < mutation_probability:
+                                allowed_mutation_types.append(mutation_type)
 
-                        allowed_mutation_types = [mutation_type
-                                                  for mutation_type, prob, real_prob in
-                                                  zip(mutation_types,
-                                                      mutation_probabilities,
-                                                      mutation_real_probabilities)
-                                                  if prob > real_prob]
+                        if allowed_mutation_types:
+                            # choose next mutation with lowest tries count
+                            next_mutation_type = min(allowed_mutation_types,
+                                                     key=lambda mutation_type:
+                                                     mutation_tries_for_each_ind[parent_descriptive_id][mutation_type])
 
-                        if not allowed_mutation_types:
-                            raise ValueError(f"Sum of mutation_probabilities is not equal to 1."
-                                             f"Check _operator_agent in mutation.")
-
-                        # get the most rare mutation for all inds
-                        stop = False
-                        lowest_mutation_count = (None, None, 0)
-                        for _graph_id, _graph_id_mutation_count in mutation_count_for_each_ind.items():
-                            for _mutation_type, _mutation_count in _graph_id_mutation_count.items():
-                                if _mutation_type in allowed_mutation_types:
-                                    if _mutation_count == 0:
-                                        lowest_mutation_count = (_graph_id, _mutation_type, _mutation_count)
-                                        stop = True
-                                    elif _mutation_count < lowest_mutation_count[-1]:
-                                        lowest_mutation_count = (_graph_id, _mutation_type, _mutation_count)
-                                if stop:
-                                    break
-                            if stop:
-                                break
-                        graph_id_to_mutate, mutation_type, _ = lowest_mutation_count
-                        mutation_queue.put((graph_id_to_mutate,
-                                            population_descriptive_ids_mapping[graph_id_to_mutate],
-                                            mutation_type))
+                            mutation_queue.put((parent_descriptive_id,
+                                                population_descriptive_ids_mapping[parent_descriptive_id],
+                                                next_mutation_type))
                 if try_num >= max_tries:
                     break
 
         self._check_final_population(new_population)
-
         return new_population
 
     def _check_final_population(self, population: PopulationT) -> None:
@@ -146,16 +133,20 @@ class ReproductionController:
                               f'have {len(population)},'
                               f' required {target_pop_size}!\n' + helpful_msg)
 
-    def _mutation_n_evaluation(self, i, mutation_queue: Queue,
+    def _mutation_n_evaluation(self,
+                               mutation_queue: Queue,
                                evaluator: EvaluationOperator):
         try:
-            # wait timeout in seconds for new task to reduce probability of process flooding
-            descriptive_id, individual, mutation_type = mutation_queue.get(timeout=1)
-            individual, mutation_type, applied = self.mutation._mutation(individual, mutation_type=mutation_type)
-            if applied and individual and self.verifier(individual.graph):
-                individuals = evaluator([individual])
-                if individuals:
-                    return descriptive_id, mutation_type, individuals[0]
+            descriptive_id, individual, mutation_type = mutation_queue.get(timeout=0.1)
         except queue.Empty:
-            pass
+            # is there is no task, then return nothing
+            return None, None, None
+
+        individual, mutation_type, applied = self.mutation._mutation(individual, mutation_type=mutation_type)
+        if applied and individual and self.verifier(individual.graph):
+            individuals = evaluator([individual])
+            if individuals:
+                # if all is ok return all data
+                return descriptive_id, mutation_type, individuals[0]
+        # if something go wrong do not return new individual
         return descriptive_id, mutation_type, None
