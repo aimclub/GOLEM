09b953e9416091919adca7ea4054bcb6454ede3f
Pinchuk Maya <mayaprr@yandex.ru>
2023-12-13 12:53:49 +0300

    docstrings for API

--- golem/api/main.py
+++ golem/api/main.py
@@ -27,6 +27,78 @@ class GOLEM:
         n_jobs: num of ``n_jobs`` for parallelization (set to ``-1`` to use all cpu's). Defaults to ``-1``.
         graph_requirements_class: class to specify custom graph requirements.
         Must be inherited from GraphRequirements class.
+
+        :param crossover_prob: crossover probability (chance that two individuals will be mated).
+
+        ``GPAlgorithmParameters`` parameters
+        :param mutation_prob: mutation probability (chance that an individual will be mutated).
+        :param variable_mutation_num: flag to apply mutation one or few times for individual in each iteration.
+        :param max_num_of_operator_attempts: max number of unsuccessful evo operator attempts before continuing.
+        :param mutation_strength: strength of mutation in tree (using in certain mutation types)
+        :param min_pop_size_with_elitism: minimal population size with which elitism is applicable
+        :param required_valid_ratio: ratio of valid individuals on next population to continue optimization.
+
+        Used in `ReproductionController` to compensate for invalid individuals. See the class for details.
+
+        :param adaptive_mutation_type: Experimental feature! Enables adaptive Mutation agent.
+        :param context_agent_type: Experimental feature! Enables graph encoding for Mutation agent.
+
+        Adaptive mutation agent uses specified algorithm. 'random' type is the default non-adaptive version.
+        Requires crossover_types to be CrossoverTypesEnum.none for correct adaptive learning,
+        so that fitness changes depend only on agent's actions (chosen mutations).
+        ``MutationAgentTypeEnum.bandit`` uses Multi-Armed Bandit (MAB) learning algorithm.
+        ``MutationAgentTypeEnum.contextual_bandit`` uses contextual MAB learning algorithm.
+        ``MutationAgentTypeEnum.neural_bandit`` uses contextual MAB learning algorithm with Deep Neural encoding.
+
+        Parameter `context_agent_type` specifies implementation of graph/node encoder for adaptive
+        mutation agent. It is relevant for contextual and neural bandits.
+
+        :param selection_types: Sequence of selection operators types
+        :param crossover_types: Sequence of crossover operators types
+        :param mutation_types: Sequence of mutation operators types
+        :param elitism_type: type of elitism operator evolution
+
+        :param regularization_type: type of regularization operator
+
+        Regularization attempts to cut off the subtrees of the graph. If the truncated graph
+        is not worse than the original, then it enters the new generation as a simpler solution.
+        Regularization is not used by default, it must be explicitly enabled.
+
+        :param genetic_scheme_type: type of genetic evolutionary scheme
+
+        The `generational` scheme is a standard scheme of the evolutionary algorithm.
+        It specifies that at each iteration the entire generation is updated.
+
+        In the `steady_state` scheme at each iteration only one individual is updated.
+
+        The `parameter_free` scheme is an adaptive variation of the `steady_state` scheme.
+        It specifies that the population size and the probability of mutation and crossover
+        change depending on the success of convergence. If there are no improvements in fitness,
+        then the size and the probabilities increase. When fitness improves, the size and the
+        probabilities decrease. That is, the algorithm choose a more stable and conservative
+        mode when optimization seems to converge.
+
+        :param decaying_factor: decaying factor for Multi-Armed Bandits for managing the profit from operators
+            The smaller the value of decaying_factor, the larger the influence for the best operator.
+        :param window_size: the size of sliding window for Multi-Armed Bandits to decrease variance.
+            The window size is measured by the number of individuals to consider.
+
+        ``GraphGenerationParams`` parameters
+        :param adapter: instance of domain graph adapter for adaptation
+         between domain and optimization graphs
+        :param rules_for_constraint: collection of constraints for graph verification
+        :param advisor: instance providing task and context-specific advices for graph changes
+        :param node_factory: instance for generating new nodes in the process of graph search
+        :param remote_evaluator: instance of delegate evaluator for evaluation of graphs
+
+        ``GraphRequirements`` parameters
+        :param start_depth: start value of adaptive tree depth
+        :param max_depth: max depth of the resulting graph
+        :param min_arity: min number of parents for node
+        :param max_arity: max number of parents for node
+
+        Also, custom domain specific parameters can be specified here. These parameters can be then used in
+        ``DynamicGraphRequirements`` as fields.
     """
     def __init__(self,
                  timeout: Optional[float] = DEFAULT_API_TIMEOUT_MINUTES,
