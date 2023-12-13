a02a4f1c1eb53f20d0b5f89d4c5991ffa830eaa0
Pinchuk Maya <mayaprr@yandex.ru>
2023-12-13 12:34:53 +0300

    minors

--- golem/api/api_utils/api_params.py
+++ golem/api/api_utils/api_params.py
@@ -2,6 +2,8 @@ from collections import UserDict
 
 from typing import Dict, Any
 
+from golem.core.adapter.nx_adapter import BaseNetworkxAdapter
+from golem.core.dag.verification_rules import DEFAULT_DAG_RULES
 from golem.core.log import LoggerAdapter, default_log
 from golem.core.optimisers.dynamic_graph_requirements import DynamicGraphRequirements
 from golem.core.optimisers.genetic.gp_optimizer import EvoGraphOptimizer
@@ -36,39 +38,53 @@ class ApiParams(UserDict):
         }
         return default_common_params
 
+    @staticmethod
+    def get_default_graph_generation_params():
+        """ Default graph generations params to minimize the number of arguments that must be specified in API.
+        Need to be hardcoded like that since the list of input arguments is not the same as the class fields list. """
+        default_graph_generation_params = {
+            'adapter': BaseNetworkxAdapter(),
+            'rules_for_constraint': DEFAULT_DAG_RULES,
+            'advisor': None,
+            'node_factory': None,
+            'random_graph_factory': None,
+            'available_node_types': None,
+            'remote_evaluator': None
+        }
+        return default_graph_generation_params
+
     def get_gp_algorithm_parameters(self) -> GPAlgorithmParameters:
         default_gp_algorithm_params_dict = dict(list(vars(GPAlgorithmParameters()).items()))
-        for k, v in self._input_params:
+        for k, v in self._input_params.items():
             if k in default_gp_algorithm_params_dict:
                 default_gp_algorithm_params_dict[k] = self._input_params[k]
-                self._input_params.pop(k)
         return GPAlgorithmParameters(**default_gp_algorithm_params_dict)
 
     def get_graph_generation_parameters(self) -> GraphGenerationParams:
-        default_graph_generation_params_dict = dict(list(vars(GraphGenerationParams()).items()))
-        for k, v in self._input_params:
+        default_graph_generation_params_dict = self.get_default_graph_generation_params()
+        for k, v in self._input_params.items():
             if k in default_graph_generation_params_dict:
                 default_graph_generation_params_dict[k] = self._input_params[k]
-                self._input_params.pop(k)
-        return GraphGenerationParams(**default_graph_generation_params_dict)
+        ggp = GraphGenerationParams(**default_graph_generation_params_dict)
+        return ggp
 
     def get_graph_requirements(self) -> GraphRequirements:
         default_graph_requirements_params_dict = dict(list(vars(GraphRequirements()).items()))
         # if there are any custom domain specific graph requirements params
         is_custom_graph_requirements_params = \
             any([k not in default_graph_requirements_params_dict for k in self._input_params])
-        for k, v in self._input_params:
+        for k, v in self._input_params.items():
             # add all parameters except common left unused after GPAlgorithmParameters and GraphGenerationParams
             # initialization, since it can be custom domain specific params
             if k not in self._default_common_params:
                 default_graph_requirements_params_dict[k] = self._input_params[k]
         if is_custom_graph_requirements_params:
-            return DynamicGraphRequirements(**default_graph_requirements_params_dict)
+            return DynamicGraphRequirements(default_graph_requirements_params_dict)
         else:
             return GraphRequirements(**default_graph_requirements_params_dict)
 
     def get_actual_common_params(self) -> Dict[str, Any]:
-        for k, v in self._input_params:
+        for k, v in self._input_params.items():
             if k in self._default_common_params:
                 self._default_common_params[k] = v
         return self._default_common_params
