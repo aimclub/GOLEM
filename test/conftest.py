33770db09ac3c973d9c73be7ffc2b62ef6b89460
Vladimir Latypov <donrumata03@gmail.com>
2024-03-30 21:45:58 +0300

    Create conftest.py to fix seed and mock urandom before all tests

---
+++ test/conftest.py
@@ -0,0 +1,23 @@
+import os
+import random
+import subprocess
+import sys
+from unittest.mock import patch
+
+import pytest
+
+from golem.utilities.utils import set_random_seed
+
+
+@pytest.fixture(autouse=True)
+def stabilize_random():
+    set_random_seed(42)
+
+    def urandom_mock(n):
+        return random.randbytes(n)
+
+    # os.random is the source of random used in the uuid library
+    # normally, it's „true“ random, but to stabilize tests,
+    # it uses seeded `random` library
+    with patch('os.urandom', urandom_mock):
+        yield
