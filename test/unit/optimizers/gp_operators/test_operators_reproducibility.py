31e2bf53aa137248ec1995d88f1a21c09cc8ef35
Вадим Ахмеров <vadimspring@mail.ru>
2025-04-30 19:11:41 +0300

    add test_operators_reproducibility

---
+++ test/unit/optimizers/gp_operators/test_operators_reproducibility.py
@@ -0,0 +1,42 @@
+from golem.core.optimisers.genetic.gp_params import GPAlgorithmParameters
+from golem.core.optimisers.genetic.operators.crossover import CrossoverTypesEnum, Crossover
+from golem.core.optimisers.opt_history_objects.individual import Individual
+from golem.core.optimisers.optimization_parameters import GraphRequirements
+from golem.core.optimisers.optimizer import GraphGenerationParams
+from golem.utilities.utilities import set_random_seed
+from test.unit.utils import graph_first, graph_second
+import pytest
+
+# subgraph crossover is non-reproducible by design
+crossover_types = [
+    CrossoverTypesEnum.subtree,
+    CrossoverTypesEnum.one_point,
+    CrossoverTypesEnum.exchange_edges,
+    CrossoverTypesEnum.exchange_parents_one,
+    CrossoverTypesEnum.exchange_parents_both
+]
+
+
+@pytest.mark.parametrize('crossover_type', crossover_types)
+def test_crossover_reproducibility(crossover_type):
+    graph_example_first = graph_first()
+    graph_example_second = graph_second()
+
+    parameters = GPAlgorithmParameters()
+    requirements = GraphRequirements()
+    graph_generation_params = GraphGenerationParams(available_node_types=['a', 'b', 'c', 'd'])
+    crossover = Crossover(parameters, requirements, graph_generation_params)
+
+    crossover.parameters.crossover_types = [crossover_type]
+
+    def run_with_seed(seed):
+        set_random_seed(seed)
+        results = crossover([Individual(graph_example_first), Individual(graph_example_second)])
+        results = [ind.graph.descriptive_id for ind in results]
+        return results
+
+    results_first = run_with_seed(42)
+    results_second = run_with_seed(42)
+
+    assert results_first == results_second
+
